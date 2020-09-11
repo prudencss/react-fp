b50a32bfb4ee71ff07baca9e9cb0b041c23c247d
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-11 16:41:13 +0200

    useBehavior hook

---
+++ src/lib/useBehavior.ts
@@ -0,0 +1,32 @@
+import { useMemo } from 'react';
+
+import {
+  EAnimation,
+  EColor,
+  EDecoration,
+  EDisabled,
+  ESize,
+  componentColor,
+  componentSize,
+  TBehavior,
+  IBehaviorProps,
+} from './Behaviors';
+
+const useBehavior = <ComponentProps extends IBehaviorProps>({
+  animation,
+  color,
+  decoration,
+  size,
+  disabled,
+} : ComponentProps): (TBehavior | string)[] => {
+  return useMemo((): (TBehavior | string)[] => [
+    animation ?? null,
+    color ? componentColor('btn')(color) : null,
+    decoration ?? null,
+    size ? componentSize('btn')(size) : null,
+    disabled ?? null,
+  ].filter(Boolean) as (TBehavior | string)[],
+  [animation, color, decoration, size, disabled]);
+}
+
+export default useBehavior;
