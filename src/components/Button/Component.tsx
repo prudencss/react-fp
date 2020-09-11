c6703feb72f8a13e74e4920f9c1843c6206a841a
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-11 16:41:52 +0200

    functional button component

--- src/components/Button/Component.tsx
+++ src/components/Button/Component.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
 
-import { Disabled as DisabledBehaviour } from '../../lib/Behaviours';
+import { IBehaviorProps } from '../../lib/Behaviors';
+import useBehavior from '../../lib/useBehavior';
 
-export interface IProps {
+export enum EButtonType = {
+  Basic = 'c-btn-basic',
+  NoBorder = 'c-btn--no-border',
+  Stroked = 'c-btn--stroked',
+  Flat = 'c-btn--flat',
+  Ghost = 'c-btn--ghost',
+};
+
+export interface IProps extend IBehaviorProps {
   /**
    * Define _button behaviour_
    * *default:* 'button'
+   * *options:* 'button', 'submit', 'reset'
    * *example:*
    * ```tsx
    * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
@@ -18,14 +28,14 @@ export interface IProps {
    *   </Button>
    * ```
    */
-  htmlType?: 'button' | 'submit' | 'reset' | undefined;
+  type?: 'button' | 'submit' | 'reset';
 
   /**
    * Add _custom styles_ as string-array class-list
    * *default:* []
    * *example:*
    * ```tsx
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
+   * import Button, { EButtonType } from '@prudencss/react-fp';
    *
    * <Button
    *   moduleSpecificClassList={['first-module__buttons--primary', 'u-hidden@xl']}
@@ -37,140 +47,52 @@ export interface IProps {
   moduleSpecificClassList?: string[];
 
   /**
-   * Add a predefined _prudencss/framework-specific animation_
-   * *default:* undefined
-   * *example:*
-   * ```tsx
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
-   * import Behaviours from '@prudencss/react-ts-fp/lib'
-   *
-   * <Button
-   *   animation={Behaviours.Animation.Ripple}
-   * >
-   *   {buttonContent}
-   * </Button>
-   * ```
+   * Add prepackaged styles per type easily
+   * *options:* ```EButtonType```
    */
-  animation?: string;
+  buttonType?: EButtonType;
 
   /**
-   * Add a predefined _prudencss/framework-sepecific color_ by _category_
-   * *default:* primary
-   * *options:* Primary, Secondary, Accent, Success, Info, Warn, Danger, Disabled, Light, Dark
-   * *example:*
-   * ```tsx
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
-   * import Behaviours from '@prudencss/react-ts-fp/lib';
-   *
-   * <Button
-   *   color={(Behaviours.ComponentColor.Primary as unknown as Function)('btn')},
-   * >
-   *   {buttonContent}
-   * </Button>
-   * ```
+   * Make the button a Floating Action Button (circular)
    */
-  color?: string;
-
-  /**
-   * Add a predefined _decorative shadow_
-   *
-   * *default:* ElevatedBottom
-   * *options:* Discrete, Elevated, ElevatedBottom, Sunken, ...
-   * *example:*
-   * ```tsx
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
-   * import Behaviours from '@prudencss/react-ts-fp/lib'
-   *
-   * <Button
-   *   decoration={Behaviours.Decoration.Elevated}
-   * >
-   *   {buttonContent}
-   * </Button>
-   * ```
-   */
-  decoration?: string;
-
-  /**
-   * Add disabled behaviour/appearance to the button
-   *
-   * 1. The _disabled_ and _aria-disabled_ html properties will be used/enabled
-   * 2. A certain helper class will be added that stops pointer events and adds a fitting pointer decoration (_not-allowed_)
-   * 3. An additional sugar css-class *disabled* will be added for your quick concerns
-   *
-   * *default:* undefined
-   * *options:* true | false
-   * *example:*
-   * ```tsx
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
-   * import Behaviours from '@prudencss/react-ts-fp/lib';
-   *
-   * <Button
-   *  disabled={true}
-   * >
-   *   {buttonContent}
-   * </Button>
-   * ```
-   */
-  disabled?: boolean;
-
-  /*
-   * Add a predefined size variation to your specific button instance
-   *
-   * *default:* (Behaviours.ComponentSize.Medium as unknown as Function)('btn')
-   * *options:* Tiny | Small | Medium | Large | Huge
-   * *example:*
-   * import Button, { IProps as IButtonProps } from '@prudencss/react-ts-fp/components';
-   * import Behaviours from '@prudencss/react-ts-fp/lib';
-   *
-   * <Button
-   *  size={(Behaviours.ComponentSize.Small as unknown as Function)('btn')}
-   * >
-   *   {buttonContent}
-   * </Button>
-   *
-   * ```tsx
-   */
-  size?: string;
-
-  /*
-   *
-   */
-  buttonType?: string;
+  fab?: boolean;
   onClick: (ev: React.SyntheticEvent<HTMLButtonElement>) => void;
   onBlur?: (ev: React.SyntheticEvent<HTMLButtonElement>) => void;
 }
 
-
-export class Component extends React.Component<IProps, IState> {
-  public render(): React.ReactNode {
-    const { onClick, onBlur,  animation, color, decoration, disabled, size, buttonType, moduleSpecificClassList, htmlType } = this.props;
-    const _disabled: string = disabled ? DisabledBehaviour : '';
-    const classList = [animation, color, decoration, size, buttonType, _disabled];
+const Button = <Props extends IBehaviorProps>(props) => {
+    const { onClick, onBlur, moduleSpecificClassList, type, buttonType, fab } = this.props;
+    const classList = classNames(
+      'c-btn',
+      buttonType,
+      { 'c-btn--fab': fab ?? false },
+      useBehavior(props),
+      moduleSpecificClassList,
+    );
     const optionalDisabledProps = disabled ? { disabled: true, "aria-disabled": true } : {};
 
-    return (
-      <button
-        type={htmlType}
-        className={`${disabled ? 'disabled' : ''} c-btn ${classList.join(' ')} ${(moduleSpecificClassList || []).join(' ')}`}
-        {...optionalDisabledProps}
-        onClick={onClick}
-        onBlur={onBlur}
-        onTransitionEnd={this.handleTransitionEndEvents}
-      >
-        {this.props.children}
-      </button >
-    );
+  handleTransitionEndEvents = (ev: React.TransitionEvent<HTMLElement>): boolean => {
+      ev.stopPropagation();
+      ev.preventDefault();
+
+      return false;
+    }
   }
 
-  protected handleTransitionEndEvents = (ev: React.TransitionEvent<HTMLElement>): boolean => {
-    ev.stopPropagation();
-    ev.preventDefault();
-
-    return false;
+  return (
+    <button
+      type={type ?? 'button'}
+      className={classList}
+      {...optionalDisabledProps}
+      onClick={onClick}
+      onBlur={onBlur}
+      onTransitionEnd={this.handleTransitionEndEvents}
+    >
+      {this.props.children}
+    </button >
+  );
   }
-}
 
-interface IState {}
 
-export default Component;
+export default Button;
 
