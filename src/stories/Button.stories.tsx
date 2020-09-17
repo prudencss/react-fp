b6643a881587d0d10c3971358080de29439277f8
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-17 17:30:22 +0200

    implementing the options as select-boxes

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
@@ -9,40 +9,46 @@ export default {
   title: 'Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    animation: { control: { type: 'select', options: Behavior.EAnimation }},
+    color: { control : { type: 'select', options: Behavior.EColor }},
+    decoration: { control : { type: 'select', options: Behavior.EDecoration}},
+    size: { control: { type: 'select', options: Behavior.ESize }},
+    disabled: { control: { type: 'select', options: Behavior.EDisabled }},
+    type: { control: { type: 'select', options: ['button', 'submit', 'reset']}},
+    buttonType: { control: { type: 'select', options: EButtonType }},
   },
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<PropsWithChildren<ButtonProps>> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   animation: Behavior.EAnimation.Ripple,
-  color: Behavior.componentColor('btn')(Behavior.EColor.Primary),
+  color: Behavior.EColor.Primary,
   decoration: Behavior.EDecoration.ElevatedBottom,
-  size: Behavior.componentSize('btn')(Behavior.ESize.Medium),
-  disabled: null,
+  size: Behavior.ESize.Medium,
+  disabled: undefined,
   type:  "button",
   moduleSpecificClassList: [],
   buttonType: EButtonType.Basic,
   fab: false,
-  onClick: null,
-  onBlur: null,
+  onClick: () => null,
+  onBlur: () => null,
   children: 'Default',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   ...Default.args,
-  color: Behavior.componentColor('btn')(Behavior.EColor.Secondary),
+  color: Behavior.EColor.Secondary,
   children: 'Secondary ...',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   ...Default.args,
-  color: Behavior.componentColor('btn')(Behavior.EColor.Success),
-  size: Behavior.componentSize('btn')(Behavior.ESize.Large),
+  color: Behavior.EColor.Success,
+  size: Behavior.ESize.Large,
   children: 'Laaarge!',
 };
 
