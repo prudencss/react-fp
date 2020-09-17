682b67a5abb704769bddcb129159691a212eca1e
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-17 17:30:02 +0200

    refactoring

--- src/lib/useBehavior.ts
+++ src/lib/useBehavior.ts
@@ -1,32 +1,29 @@
 import { useMemo } from 'react';
 
 import {
-  EAnimation,
-  EColor,
-  EDecoration,
-  EDisabled,
-  ESize,
   componentColor,
   componentSize,
   TBehavior,
   IBehaviorProps,
 } from './Behaviors';
 
-const useBehavior = <ComponentProps extends IBehaviorProps>({
-  animation,
-  color,
-  decoration,
-  size,
-  disabled,
-} : ComponentProps): (TBehavior | string)[] => {
-  return useMemo((): (TBehavior | string)[] => [
-    animation ?? null,
-    color ?? null,
-    decoration ?? null,
-    size ?? null,
-    disabled ?? null,
-  ].filter(Boolean) as (TBehavior | string)[],
-  [animation, color, decoration, size, disabled]);
-}
+const useBehavior = <ComponentProps extends IBehaviorProps>(
+  componentInfix: string,
+  {
+    animation,
+    color,
+    decoration,
+    size,
+    disabled,
+  } : ComponentProps): (TBehavior | string)[] => {
+    return useMemo((): (TBehavior | string)[] => [
+      animation ?? null,
+      color ? componentColor(componentInfix)(color) : null,
+      decoration ?? null,
+      size ? componentSize(componentInfix)(size) : null,
+      disabled ?? null,
+    ].filter(Boolean) as (TBehavior | string)[],
+    [componentInfix, animation, color, decoration, size, disabled]);
+  }
 
 export default useBehavior;
