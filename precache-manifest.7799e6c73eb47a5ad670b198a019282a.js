0880b58ca02a18c0142c2c43c090133158b489e9
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-11-23 07:28:07 +0100

    Updates

---
+++ precache-manifest.7799e6c73eb47a5ad670b198a019282a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eb7ac2ea45a2fc4cd4ec03074f6e51",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.65885d9614680b281954.bundle.js"
+  },
+  {
+    "url": "runtime~main.65885d9614680b281954.bundle.js"
+  },
+  {
+    "revision": "25c24f3a75d868f2ebce",
+    "url": "static/css/main.fcc6775e.chunk.css"
+  },
+  {
+    "revision": "ec02cb1c052e1f84d243",
+    "url": "static/css/vendors~main.56c80c15.chunk.css"
+  },
+  {
+    "revision": "2eb1a85c02e850c658bdccecd96d8672",
+    "url": "static/media/index.2eb1a85c.svg"
+  },
+  {
+    "url": "vendors~main.65885d9614680b281954.bundle.js"
+  },
+  {
+    "url": "vendors~main.65885d9614680b281954.bundle.js.LICENSE.txt"
+  }
+]);
