fc35d11516db7fc329ca83c3c6e19bfb7adccbdc
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2023-07-01 21:12:55 -0400

    feat(flexbox): forgot public export;

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * as List from "./components/List";
 export * as Nav from "./components/Nav";
 export * as Root from "./components/Root";
 export * as Layout from "./components/Layout";
+export * as Flexbox from "./components/Flexbox";
 export * from "./constants";
 export * from "./enums";
 export * from "./utils";
